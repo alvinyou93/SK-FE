a8e3d79a0efcace51d4f26535489426d29cab5e7
alvinyou93 <lkjdk2486@naver.com>
2022-05-16 14:23:48 +0900

    temp

--- coderv/src/App.js
+++ coderv/src/App.js
@@ -1,23 +1,11 @@
+import { ClassComponent, A11yHidden } from "./components";
 import logo from "./logo.svg";
 import "./App.css";
 
 export function COUNT_UP() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
@@ -26,18 +14,8 @@ export function BUTTON_COUNT() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ClassComponent />
+        <A11yHidden>Hey</A11yHidden>
       </header>
     </div>
   );
